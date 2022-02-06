0c665d6a0b99805a8baa9a43c64c856602edc985
Beruk <89202258+berukgans@users.noreply.github.com>
2022-02-06 13:09:02 +0700

    Add files via upload

---
+++ gcphone/config.lua
@@ -0,0 +1,11 @@
+FixePhone = {
+  -- Poste de police
+  ['911'] = { name =  "Central Police", coords = { x = 441.2, y = -979.7, z = 30.58 } },
+  
+  -- Cabine proche du poste de police
+  ['008-0001'] = { name = "Cabine Telephonique", coords = { x = 372.25, y = -965.75, z = 28.58 } },
+}
+
+ShowNumberNotification = true -- Show Number or Contact Name when you receive new SMS
+
+--set discord_webhook 'https://discordapp.com/api/webhooks/698004763090092112/R3Z-ohPJBKIW3wFzSCvDhNOsIZfh3mSFaoeCosziHnA7eqsYPOIBhlQ-tVIuuyHSCBIg'
